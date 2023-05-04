cc2df2fc24e15fef7576c4b0ec7905c28eb5d921
Davide Nasi <davide.nasi.intern@nielsen.com>
2023-05-04 09:23:40 +0200

    test script

--- scripts/wolves/test.sh
+++ scripts/wolves/test.sh
@@ -3,6 +3,10 @@
 set -x
 set -e
 
+cd lib
+python3.7 setup.py build develop
+cd ..
+
 # 46.3(10)
 CUDA_VISIBLE_DEVICES=0 python test_net.py --dataset wolves --net res101 --cuda \
---model_dir models/wolves/res101/TIA/da_faster_rcnn_wolves_10.pth
+--model_dir models/wolves/res101/TIA/da_faster_rcnn_wolves_12.pth
