f158c5cd2e7be7824800292807829c64308620b4
Davide Nasi <davide.nasi.intern@nielsen.com>
2023-05-10 14:54:21 +0200

    run on docker

--- scripts/wolves/train_no_da.sh
+++ scripts/wolves/train_no_da.sh
@@ -7,4 +7,4 @@ cd lib
 python3.7 setup.py build develop
 cd ..
 
-CUDA_VISIBLE_DEVICES=0 python3.7 train_no_da.py --dataset wolves --net res101 --cuda --net res101 --desp 'no_da' --epochs 12
+CUDA_VISIBLE_DEVICES=0 python3.7 train_no_da.py --dataset wolves --net res101 --cuda --net res101 --epochs 12
