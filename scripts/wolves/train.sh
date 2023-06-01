a8e02d91251e1e06d4386bdf9d8b62200b38be78
Davide Nasi <davide.nasi.intern@nielsen.com>
2023-06-01 10:28:21 +0200

    train on scaled images

--- scripts/wolves/train.sh
+++ scripts/wolves/train.sh
@@ -7,7 +7,7 @@ cd lib
 python3.7 setup.py build develop
 cd ..
 
-CUDA_VISIBLE_DEVICES=0 python3.7 trainval_net.py --dataset wolves --net resnet101 --cuda \
+CUDA_VISIBLE_DEVICES=0 python3.7 trainval_net.py --dataset wolves --net res101 --cuda \
 --epochs 10 --gamma 5.0 --warmup --context --lr 0.00001 \
 --alpha1 1.0 --alpha2 1.0 --alpha3 1.0 \
 --lamda1 1.0 --lamda2 1.0 --lamda3 0.01 \
